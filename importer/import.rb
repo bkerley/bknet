d20cdff74cb875f7dab978830167d1ed10fa3330
Bryce Kerley <bkerley@brycekerley.net>
2009-01-02 23:38:42 -0500

    put excerpt in right place

--- importer/import.rb
+++ importer/import.rb
@@ -8,7 +8,7 @@ Post.all.each do |p|
 	print p.title
 	a = p.attributes
 	a.delete 'body'
-	a.delete 'excerpt'
+	# a.delete 'excerpt'
 	a.delete 'tags'
 	a['blog_post'] = true
 	a['filter'] = ['markdown']
