cf02c3fdab708d47e28d0e5bed59ddd7aafab793
Bryce Kerley <bkerley@brycekerley.net>
2009-01-02 22:56:45 -0500

    import everything

--- importer/import.rb
+++ importer/import.rb
@@ -4,19 +4,25 @@ require 'active_record'
 ['post', 'tag'].each{|f|require File.dirname(__FILE__)+"/#{f}.rb"}
 ActiveRecord::Base.establish_connection(:adapter=>'sqlite3', :dbfile=>File.dirname(__FILE__)+'/production.sqlite3')
 
-p = Post.first
-a = p.attributes
-a.delete 'body'
-a.delete 'excerpt'
-a.delete 'tags'
-a['blog_post'] = true
-a['filter'] = ['textile']
-a['tags'] = p.tag_string
-header = a.to_yaml
-path = File.dirname(__FILE__)+'/../content/blog/'+p.created_at.strftime("%Y/%m/%d")
-filename = path +"/#{p.slug}.txt"
-FileUtils.mkdir_p path
-File.open(filename, 'w') do |f|
-	f.puts header
-	f.puts p.body.to_yaml
+Post.all.each do |p|
+	print p.title
+	a = p.attributes
+	a.delete 'body'
+	a.delete 'excerpt'
+	a.delete 'tags'
+	a['blog_post'] = true
+	a['filter'] = ['markdown']
+	a['tags'] = p.tag_string
+	header = a.to_yaml
+	path = File.dirname(__FILE__)+'/../content/blog/'+p.created_at.strftime("%Y/%m/%d")
+	filename = path +"/#{p.slug}.txt"
+	print '.'
+	FileUtils.mkdir_p path
+	print '.'
+	File.open(filename, 'w') do |f|
+		f.puts header
+		f.puts '--- '
+		f.puts p.body
+	end
+	puts '.Ok'
 end
